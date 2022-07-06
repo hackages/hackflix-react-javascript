cc8139b1b39154e6bf6da03dc44e8b50341f5d0c
Micha Schoone <mc.schoone@avans.nl>
2022-07-06 08:53:12 +0200

    item list component build, custom styles added

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,14 @@
 export function Item({ item }) {
   return (
     <div className="single-movie relative">
-      <span className="text-3xl font-bold">{item.title}</span>
+      <img src={item.poster_path} />
+      <div className="movie-content flex items-center justify-center items-center justify-center text-center absolute w-full h-full inset-0 px-4">
+        <div className="content-inner">
+          <button class="bookmark p-1 inline-block absolute">Bookmark</button>
+          <h3 class="mb-5">{item.title}</h3>
+          <a class="details-btn inline-block" href={item.id}>View Details</a>
+        </div>
+      </div>
     </div>
   )
 }
