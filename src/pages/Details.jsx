15fb3f1607a61cfa7f5bbafcc57001eb1fd72ade
Micha Schoone <mc.schoone@avans.nl>
2022-07-11 13:58:49 +0200

    rounded corner image detail page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -22,14 +22,14 @@ export function DetailsPage() {
       <div className="movie-list py-20">
         <div className="container mx-auto">
           <div className="flex items-center">
-            <div className='details-image mr-5'>
+            <div className='details-image mr-5 rounded-md overflow-hidden'>
               <img src={movie.poster_path} />
             </div>
             <div className="details-content ml-5">
               <h1 className="mb-5 text-4xl font-bold">{movie.title}</h1>
               <p className="text-sm opacity-75 mb-5">{movie.overview}</p>
               <p className='mb-5'>{genresNames.join(' / ')}</p>
-              <button className="bg-red-700 hover:bg-red-900 text-white py-3 px-8 rounded transition-all duration-200 inline-block">Watch</button>
+              <button className="bg-red-600 hover:bg-red-900 text-white py-3 px-8 rounded transition-all duration-200 inline-block">Watch</button>
             </div>
           </div>
         </div>
