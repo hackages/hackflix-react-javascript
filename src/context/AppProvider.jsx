f9efa639d753a014ecde356c671577d843335c4b
Rob Funcken <mail@robfuncken.nl>
2022-07-11 14:15:31 +0200

    fix

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -2,27 +2,21 @@ import { createContext, useState } from "react";
 
 import { filterMovieByTitle, filterMovieByCategory } from "../utils";
 
-import { movies, categories } from "../../mocks";
-import { filterMovieByTitle } from "../utils/filterMoviesByTitle";
-import { getLocalStorage, setLocalStorage } from "../utils/localStorageHelper";
+import { movies } from "../../mocks";
+import { getLocalStorage } from "../utils/localStorageHelper";
 
 export const AppContext = createContext();
 
 export function AppProvider({ children }) {
-
-  const initialSearchQuery = getLocalStorage("searchTerm");
-  const [searchQuery, setSearchQuery] = useState(initialSearchQuery || "");
+  const [searchQuery, setSearchQuery] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("Action");
 
-    const [moviesToShow, updateMovies] = useState(movies);
-
-
+  const [moviesToShow, updateMovies] = useState(movies);
 
   function filterByTitle(searchTerm) {
     const filteredMovies = filterMovieByTitle(movies, searchTerm);
     updateMovies(filteredMovies);
   }
-
 
   function filterByCategoryAndTitle(searchParams) {
     console.log(searchParams);
@@ -35,21 +29,28 @@ export function AppProvider({ children }) {
       const filteredMovies = filterMovieByCategory(movies, categories);
       updateMovies(filteredMovies);
     }
-
+  }
 
   function getMovieById(id) {
-    return movies.find(movie => movie.id === parseInt(id))}
+    return movies.find((movie) => movie.id === parseInt(id));
+  }
   function selectFilter(selectedFilter) {
-    setSelectedCategory(selectedFilter)
-
-
+    setSelectedCategory(selectedFilter);
   }
 
   return (
     <AppContext.Provider
-      value={{ movies: moviesToShow, filterByTitle, searchQuery, selectFilter, selectedCategory, filterByCategoryAndTitle, filterByCategory, filterByTitle, getMovieById }}
-
-
+      value={{
+        movies: moviesToShow,
+        filterByTitle,
+        searchQuery,
+        selectFilter,
+        selectedCategory,
+        filterByCategoryAndTitle,
+        filterByCategory,
+        filterByTitle,
+        getMovieById,
+      }}
     >
       {children}
     </AppContext.Provider>
