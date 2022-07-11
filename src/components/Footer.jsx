04e4fb18dd85dd77a52ab262c7b77dbc8609cdf4
Niels-vb <nj.vanberkel@avans.nl>
2022-07-11 09:30:41 +0200

    Netlify test2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export function Footer(){
     const year = new Date().getFullYear();
     return (
         <footer className="py-6 bg-gray-900 text-center">
-            <p className="text-white">Hackflicx © {year} Powered by Avans</p>
+            <p className="text-white">Hackflix © {year} Powered by Avans</p>
         </footer>
     )
 }
