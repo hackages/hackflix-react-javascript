a40ca48e5aab53d4055ff5111799d173351ab9c6
Niels-vb <nj.vanberkel@avans.nl>
2022-07-11 10:08:58 +0200

    Added animation and bookmark styling

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,3 +1,18 @@
+import { useContext } from "react";
+import { ItemsList } from "../components/ItemsList";
+import { AppContext } from "../context/AppProvider";
+
 export function BookmarksPage(){
-  return <h1>Bookmars page</h1>
+  const { movies } = useContext(AppContext);
+
+  return (
+    <div className="movie-list py-20">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
+          <ItemsList data={movies} />
+          <button class="remove-bookmark block text-center py-2 mt-4 w-full">Remove Bookmark</button>
+        </div>
+      </div>
+    </div>
+  );
 }
