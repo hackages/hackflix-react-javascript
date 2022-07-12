404615f2276bb84f0c113418643cc9739ee73377
Davy Engone <davy@hackages.io>
2022-07-12 16:00:40 +0200

    get movie by id

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -5,17 +5,19 @@ import { useServerData } from "../hooks/useServerData";
 
 export function DetailsPage() {
   const params = useParams();
-  const movieId = params.movieId;
-  const { getMovieById } = useContext(AppContext);
-  const movie = getMovieById(movieId);
-  const { genres, movies } = useServerData();
-  const genresNames = movies[0].genre_ids.map((genreId) => {
-    const genre = genres.find((g) => g.id === genreId);
-    if (genre) {
-      return genre.name;
-    }
-    return null;
-  });
+  const { selectedMovie: movie } = useServerData(params.movieId);
+  console.log(movie);
+
+
+  // const genresNames = movies[0].genre_ids.map((genreId) => {
+  //   const genre = genres.find((g) => g.id === genreId);
+  //   if (genre) {
+  //     return genre.name;
+  //   }
+  //   return null;
+  // });
+
+  if(!movie) return
 
   let navigate = useNavigate();
   function homeLink() {
@@ -33,7 +35,7 @@ export function DetailsPage() {
             <div className="details-content md:ml-5">
               <h1 className="mb-5 text-4xl font-bold">{movie.title}</h1>
               <p className="text-sm opacity-75 mb-5">{movie.overview}</p>
-              <p className="mb-5">{genresNames.join(" / ")}</p>
+              {/* <p className="mb-5">{genresNames.join(" / ")}</p> */}
               <button className="bg-red-600 hover:bg-red-900 text-white py-3 px-8 rounded transition-all duration-200 inline-block">
                 Watch
               </button>
