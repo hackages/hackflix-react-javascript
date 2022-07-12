a730e1646176239e3c796d066b5a7f4d1bd459e2
Rob Funcken <mail@robfuncken.nl>
2022-07-12 14:55:14 +0200

    added genres and categories from server

--- src/hooks/useServerData.js
+++ src/hooks/useServerData.js
@@ -3,23 +3,49 @@ import { useState } from "react";
 
 export function useServerData() {
   const [movies, setMovies] = useState([]);
+
   const [fetching, setFetching] = useState(false);
   const [initialMovies, setInitialMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     setFetching(true);
-    fetch("https://hackflix-api-with-nextjs.vercel.app/api/movies")
-      .then((response) => response.json())
+
+    const urls = [
+      "https://hackflix-api-with-nextjs.vercel.app/api/movies",
+      "https://hackflix-api-with-nextjs.vercel.app/api/genres",
+      "https://hackflix-api-with-nextjs.vercel.app/api/categories",
+    ];
+
+    const requests = urls.map((url) => fetch(url));
+
+    Promise.all(requests)
+      .then((responses) => {
+        return responses.map((response) => response.json());
+      })
       .then((data) => {
-        setMovies(data.movies);
-        setInitialMovies(data.movies);
+        // setMovies(data[0].then(data => data.movies));
+        /*       setInitialMovies(data[0]);
+        setGenres(data[1]);
+
+        setCategories(data[2]); */
+        data[0].then((data) => setMovies(data.movies));
+        data[0].then((data) => setInitialMovies(data.movies));
+        data[1].then((data) => setGenres(data.genres));
+        data[2].then((data) => setCategories(data.categories));
+        console.log(categories);
         setFetching(false);
       });
   }, []);
+  console.log(categories);
 
   return {
     initialMovies,
     movies,
     setMovies,
     fetching,
+    genres,
+    categories,
   };
 }
